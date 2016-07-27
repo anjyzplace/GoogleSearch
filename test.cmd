7ae01bea073524b4a9c0fae72569de355ca6840c
anjyzplace <anjola.awofisoye@gmail.com>
2016-07-27 13:28:10 +0100

    updated script

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
 echo "Testing Application"
-C:\Program Files\NUnit-3.2.1\bin\nunit3-console.exe GoogleSearch\GoogleSearch\bin\Debug\GoogleSearch.dll --out=TestOutput.txt --work=TestResult.xml
+"C:\Program Files\NUnit-3.2.1\bin\nunit3-console.exe" GoogleSearch\GoogleSearch\bin\Debug\GoogleSearch.dll --out=TestOutput.txt --work=TestResult.xml
