25ac571312ba61d46b94abe1ab10cbd8910af2e8
anjyzplace <anjola.awofisoye@gmail.com>
2016-08-16 21:23:02 +0100

    updated Selenium Chrome

--- GoogleSearch/GoogleSearch/UnitTest1.cs
+++ GoogleSearch/GoogleSearch/UnitTest1.cs
@@ -1,51 +1,49 @@
-﻿
-namespace GoogleSearch
+﻿namespace GoogleSearch
 {
-    using System;
-    using System.Threading;
     using NUnit.Framework;
-    using NUnit.Framework.Internal;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
+    using System;
 
     [TestFixture]
     public class UnitTest1
     {
-        string expect = "Google";
-        string actual = null;
-        IWebDriver driver = new ChromeDriver();
+        public string Expect = "Google";
+        public string Actual = "Hello";
+        private readonly IWebDriver _driver = new ChromeDriver();
+
         [Test]
         public void OpenPage()
         {
- 
-            driver.Navigate().GoToUrl("http://www.google.com");
+            _driver.Navigate().GoToUrl("http://www.google.com");
         }
 
         [Test]
         public void TypeInSearch()
         {
             string item = "Banana";
-            driver.FindElement(By.XPath("//*[@id='lst-ib']")).SendKeys(item);
-            driver.FindElement(By.XPath(".//*[@id='sblsbb']/button")).Click();
-            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(2));
-            actual = driver.Title;
-            Console.WriteLine("Actual Page Title is " + actual);
-            Assert.IsTrue(expect == actual);
+            _driver.FindElement(By.XPath("//*[@id='lst-ib']")).SendKeys(item);
+            _driver.FindElement(By.XPath(".//*[@id='sblsbb']/button")).Click();
+            _driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(2));
+            Actual = _driver.Title;
+            Console.WriteLine("Actual Page Title is " + Actual);
+            Assert.IsTrue(Expect == Actual);
         }
 
         [Test]
         public void GetPageTitile()
         {
+            int a = 1;
+            int b = 1;
 
-            Assert.IsTrue(1 == 1);
-
+            Assert.IsTrue(a == b);
         }
 
-        [TestFixtureTearDown]
+        [OneTimeTearDown]
         public void Close()
         {
-            driver.Close();
-            driver.Quit();
+            _driver.Close();
+            _driver.Quit();
         }
     }
 }
